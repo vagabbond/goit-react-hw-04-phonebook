7bc37ebe318e7f54a2923b88c9d3340e1e7f26cb
Maks Thimbarov <imaksim2@icloud.com>
2023-01-28 15:45:53 +0200

    done

--- src/components/phonebook/Phonebook.jsx
+++ src/components/phonebook/Phonebook.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
@@ -11,62 +11,52 @@ import {
   Button,
 } from './Phonebook.styled.jsx';
 
-class Phonebook extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const Phonebook = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ ...this.state, id: nanoid() });
-    this.reset();
+    onSubmit({ name, number, id: nanoid() });
+    reset();
   };
-  reset = () => {
-    return this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
-  render() {
-    return (
-      <PhonebookContainer>
-        <Title>Phonebook</Title>
-        <Form onSubmit={this.handleSubmit}>
-          <Label htmlFor="name">
-            Name
-            <Input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </Label>
-          <Label>
-            Telephone
-            <Input
-              type="tel"
-              name="number"
-              value={this.state.number}
-              onChange={this.handleChange}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            ></Input>
-          </Label>
-          <Button type="submit">Add contact</Button>
-        </Form>
-      </PhonebookContainer>
-    );
-  }
-}
+  return (
+    <PhonebookContainer>
+      <Title>Phonebook</Title>
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor="name">
+          Name
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label>
+          Telephone
+          <Input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={e => setNumber(e.target.value)}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          ></Input>
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </PhonebookContainer>
+  );
+};
 
 Phonebook.propTypes = {
   onSubmit: PropTypes.func.isRequired,
